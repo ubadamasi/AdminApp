8324ccb9a28f30f35cfc716d46f7cff9d6a048b8
Usman Badamasi <lil_hommy_3000@hotmail.com>
2019-01-09 12:55:28 +0100

    Removed-MVCApp

---
+++ Admin.App.MVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Admin.App.MVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Admin.App.MVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Admin.App.MVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
--- Admin.App.MVC/Models/Employee.cs
+++ Admin.App.MVC/Models/Employee.cs
@@ -12,15 +12,12 @@ namespace Admin.App.MVC.Models
         public string Surname { get; set; }
         public string OtherNames { get; set; }
         public Gender Gender { get; set; }
-        public int GenderId { get; set; }
         public DateTime BirthDate { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public DateTime DateHired { get; set; }
         public string Address { get; set; }
         public State State { get; set; }
-        public int StatedId { get; set; }
         public LocalGovernment LocalGovernment { get; set; }
-        public int LocalGovernmentId { get; set; }
     }
 }
