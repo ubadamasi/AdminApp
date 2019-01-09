0cc041ade8e9002c0df64068fbb21e127f954c00
Usman Badamasi <lil_hommy_3000@hotmail.com>
2019-01-09 12:20:02 +0100

    New MVC App

---
+++ Admin.App.MVC/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Admin.App.MVC.Models
+{
+    public class Gender
+    {
+        public byte Id { get; set; }
+        public string Name { get; set; }
+    }
+}
